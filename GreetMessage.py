32572e187cd7f6697ce40330cbf2527553756fa5
Soren S <soren.is@hotmail.ca>
2022-02-21 20:40:28 -0800

    Create GreetMessage.py
    Created GreetMessage.py, adding the greetMessage function.

---
+++ GreetMessage.py
@@ -0,0 +1,11 @@
+from random import random
+
+
+def greetMessage():
+    greetMessages = []
+    greetMessages.append("Hello.")
+    greetMessages.append("How do you do?")
+    greetMessages.append("Hi!")
+    greetMessages.append("Good day.")
+    randomMessage = round(random()*5)
+    return greetMessages[randomMessage]
