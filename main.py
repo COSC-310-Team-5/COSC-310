ae6dae08a4e45eb9e5125a19a2217908b1c4cb33
N0tGoingPro <87959691+N0tGoingPro@users.noreply.github.com>
2022-02-24 12:09:01 -0800

    Update main.py add Bot to error message

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Main:
     print(f"Bot: {GreetMessage.greetMessage()}")
     userInput = input("User: ")
     while((not userInput.replace(' ','').isalpha()) or (len(userInput.split()) == (not 1)) ):
-        print("Please try again, remember to use only letters.")
+        print("Bot: Please try again, remember to use only letters.")
         userInput = input("User: ")
     print(f"Bot: {GettingStarted.gettingStarted()}")
     userWantsToTalk = True
@@ -17,7 +17,7 @@ class Main:
     while(userWantsToTalk):        
         userInputSentence = input("User: ")
         while((not userInputSentence.replace(' ','').isalpha()) or (len(userInputSentence) == 0) ):
-            print("Please try again, remember to use only letters.")
+            print("Bot: Please try again, remember to use only letters.")
             userInputSentence = input("User: ")
         if(len(userInputSentence.split())<=2):
             print(f"Bot: {GoodbyeMessage.goodbyeMessage()}")
@@ -25,7 +25,7 @@ class Main:
         else:
             botAnswer,correctnessValue = BotRespons.bot_respons(userInputSentence,databaseInList)
             if correctnessValue > 1 or correctnessValue <= (1/3):
-                print("Bot: I'm sorry, that seems a little complex for me. Could you say it a little more simply please?")
+                print("Bot: I'm sorry, I cannot understand that sentence. Could you say it a little more simply please?")
             else:
                 print(f"Bot: {botAnswer}")
             correctnessValue = 0
